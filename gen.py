b727ab69b6e164f3691b471c5034ae787526cbdc
Maksym Siverskyi <m.siverskyi@yukonww.com>
2026-04-14 16:55:29 +0300

    T

--- gen.py
+++ gen.py
@@ -7,6 +7,7 @@ import sys
 
 def main():
     parser = argparse.ArgumentParser(description="Nginx config generator from TOML")
+    parser.add_argument("config", help="Path to the J2 template file (e.g., nginx.conf.j2)")
     parser.add_argument("input", help="Path to input TOML file (e.g., apps.toml)")
     parser.add_argument("output", help="Path to output Nginx config (e.g., nginx.conf)")
     
@@ -20,7 +21,7 @@ def main():
         # 2. Initialize Jinja2 (look for templates in the current directory)
         env = Environment(loader=FileSystemLoader('.'))
         # The template file name can also be passed as an argument if needed
-        template = env.get_template('nginx.conf.j2')
+        template = env.get_template(args.config)
 
         # 3. Render the data into the template
         rendered_config = template.render(
